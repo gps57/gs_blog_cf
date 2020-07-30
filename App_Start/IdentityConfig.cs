6a44a2814b192633d240d06edd6b796223cf3329
gps57 <glen@swiftbt.com>
2020-07-30 15:56:41 -0400

    Updated Git Ignore
    Also added email service.

--- App_Start/IdentityConfig.cs
+++ App_Start/IdentityConfig.cs
@@ -11,6 +11,9 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using gs_blog_cf.Models;
+using System.Net.Mail;
+using System.Web.Configuration;
+using System.Net;
 
 namespace gs_blog_cf
 {
@@ -20,6 +23,35 @@ namespace gs_blog_cf
         {
             // Plug in your email service here to send an email.
             return Task.FromResult(0);
+        }
+
+        public async Task SendAsync(MailMessage message)
+        {
+            var GmailUsername = WebConfigurationManager.AppSettings["username"];
+            var GmailPassword = WebConfigurationManager.AppSettings["password"];
+            var host = WebConfigurationManager.AppSettings["host"];
+            int port = Convert.ToInt32(WebConfigurationManager.AppSettings["port"]);
+
+            using (var smtp = new SmtpClient()
+            {
+                Host = host,
+                Port = port,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(GmailUsername, GmailPassword)
+            })
+            {
+                try
+                {
+                    await smtp.SendMailAsync(message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    await Task.FromResult(0);
+                }
+            };
         }
     }
 
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using gs_blog_cf.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -34,8 +37,44 @@ namespace gs_blog_cf.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+            EmailModel model = new EmailModel();
+            return View(model);
+        }
 
-            return View();
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var body = "<p>Email From: <bold>{0}</bold>({1})</p><p>Message:</p><p>{2}</p>";
+                    var from = "MyPortfolio<example@email.com";
+                    model.Body = "This is a message from your blog site.  The name and the email of the contacting person is above.";
+
+                    var sendTo = ConfigurationManager.AppSettings["emailto"];
+
+                    var email = new MailMessage(from, sendTo)
+                    {
+                        Subject = "Blog Contact Message",
+                        Body = "A test string for the body of the email.",
+                        //Body = string.Format(body, model.FromName, model.FromEmail, model.Body),
+                        IsBodyHtml = true
+                    };
+
+                    var svc = new EmailService();
+                    await svc.SendAsync(email);
+
+                    return View(new EmailModel());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    await Task.FromResult(0);
+                }
+            }
+            return View(model);
         }
     }
 }
